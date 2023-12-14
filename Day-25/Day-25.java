ce7275c9a2baf70191997e513709d9dc6065280c
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2023-12-14 19:41:34 +0530

    Update Day-25.java

--- Day-25/Day-25.java
+++ Day-25/Day-25.java
@@ -1,1 +1,45 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head==null || head.next==null) {
+            return head;
+        }
+        if (check(head,k)) {    //Function to check if we have k more nodes after head node.
+            return head;    //if lessthan k nodes just return it
+        }
+        ListNode th = head;
+        ListNode dummy = null;
+        ListNode t = head;
+        int count = k;
+        while (count>0 && head!=null) {
+            t = head;
+            head = head.next;
+            t.next = dummy;
+            dummy = t;
+            count--;
+        }
+        th.next = head;
+        ListNode x = reverseKGroup(head,k);
+        th.next = x;
+        return dummy;
+    }
+    public boolean check(ListNode head, int k) {
+        while(k>0 && head!=null) {
+            head = head.next;
+            k--;
+        }
+        if (k==0) {
+            return false;
+        }
+        return true;
+    }
+}
