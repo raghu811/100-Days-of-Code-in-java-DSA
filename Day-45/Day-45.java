86493bad5662a46a3f8ad3f5d25fc226cf860cea
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-01-03 20:02:44 +0530

    Update Day-45.java

--- Day-45/Day-45.java
+++ Day-45/Day-45.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public int jump(int[] nums) {
+         int n = nums.length,ans = 0, curr = 0, y = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            y = Math.max(y, i + nums[i]);
+            if (i == curr) {
+                ans++;
+                curr = y;
+                if (curr >= n - 1) break; 
+            }
+        }
+        return ans;
+    }
+}
