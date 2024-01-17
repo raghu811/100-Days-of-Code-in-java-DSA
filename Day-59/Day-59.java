916bcb8c3ef9b85256b5be9d348570c34092429d
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-01-17 19:15:32 +0530

    Update Day-59.java

--- Day-59/Day-59.java
+++ Day-59/Day-59.java
@@ -1,1 +1,31 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
 
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+}
