76dbf1889c98f182faadac5bb1b3540fae118a49
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-01-20 19:52:55 +0530

    Update Day-62.java

--- Day-62/Day-62.java
+++ Day-62/Day-62.java
@@ -1,1 +1,22 @@
 
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        return remove(head);   
+    }
+    static ListNode remove(ListNode  head){
+        if(head==null || head.next == null) return head;
+        var si=head;
+        var ei = head;
+        while(ei!=null){
+            if(si.val != ei.val){
+                si.next = ei;
+                si=si.next;
+            }
+            ei = ei.next;
+        }
+        if(si != ei){
+            si.next = ei;
+        }
+        return head;
+    }
+}
