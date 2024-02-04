d701927e54c6cf8dc86a0b43b6ef73ec3ad1a05b
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-02-04 19:56:33 +0530

    Update Day-77.java

--- Day-77/Day-77.java
+++ Day-77/Day-77.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    private void dfs(TreeNode node, List<Integer> path, int remainingSum) {
+        // if it is null, then return
+        if (node == null) return;
+        path.add(node.val);
+        if (node.left == null && node.right == null && remainingSum == node.val) ans.add(new ArrayList<>(path));
+        this.dfs(node.left, path, remainingSum - node.val);
+        this.dfs(node.right, path, remainingSum - node.val);
+        // backtrack 
+        path.remove(path.size() - 1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Integer> path = new ArrayList<Integer>();
+        dfs(root, path, targetSum);
+        return ans;
+    }
+}
