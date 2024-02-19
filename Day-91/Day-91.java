5b6fac8f841f903a22d2e1bdbbd84c53df7fea42
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-02-19 20:13:27 +0530

    Update Day-91.java

--- Day-91/Day-91.java
+++ Day-91/Day-91.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    List<List<Integer>> res;
+    void solve(Node cur, int level){
+        if(cur == null)
+            return ;
+        if(level >= res.size()){
+            res.add(new ArrayList<Integer>());
+        }
+        res.get(level).add(cur.val);
+        for(int i=0;i<cur.children.size();i++){
+            solve(cur.children.get(i), level+1);
+        }
+    }
+    public List<List<Integer>> levelOrder(Node root) {
+        res = new ArrayList<>();
+        solve(root, 0);
+        return res;
+    }
+}
