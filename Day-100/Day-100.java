3847e5b1dc003eee604666b2aac527c30035f1e0
Raghunandan Tomar <101514269+raghu811@users.noreply.github.com>
2024-02-29 21:18:00 +0530

    Update Day-100.java

--- Day-100/Day-100.java
+++ Day-100/Day-100.java
@@ -1,1 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
 
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private void preOrder(Node root, List<Integer> list) {
+        list.add(root.val);
+        for (Node child : root.children) {
+            if (child != null) {
+                preOrder(child, list);
+            }
+        }
+    }
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            preOrder(root, list);
+        }
+        return list;
+    }
+}
